c55d843749ae2f3c06643539d7bebb02a80103a2
Egor Shmorgun <egor.shmorgun@opensesame.com>
2023-04-04 19:51:20 +0300

    add docs to CI

--- docs/make.jl
+++ docs/make.jl
@@ -13,9 +13,4 @@ makedocs(
     ]
 )
 
-# Documenter can also automatically deploy documentation to gh-pages.
-# See "Hosting Documentation" and deploydocs() in the Documenter manual
-# for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+deploydocs(repo = "github.com/OpenSesame/SegmentAnalytics.jl.git")
